0655e101595c1e83dcf6bee02f97c391030a6a8e
Yihao Chen <15yc9@queensu.ca>
2020-08-14 05:06:27 -0400

    Adding allowed hosts for easy hosting..

--- stack_overseer/stack_overseer/settings.py
+++ stack_overseer/stack_overseer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+ALLOWED_HOSTS = ['*']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
